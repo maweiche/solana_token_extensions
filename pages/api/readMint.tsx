01a070bbbb1b6ec729f3a135798529d88db2ba5b
Matt <maweiche@gmail.com>
2024-01-28 12:03:17 +0100

    cleaned up readMint

--- pages/api/readMint.tsx
+++ pages/api/readMint.tsx
@@ -4,15 +4,11 @@ import {
   PublicKey
 } from "@solana/web3.js";
 import {
-  TOKEN_2022_PROGRAM_ID,
-  getMint,
-  getMetadataPointerState,
   getTokenMetadata,
 } from "@solana/spl-token";
 import {
   TokenMetadata,
 } from "@solana/spl-token-metadata";
-
 
 type Data = {
   metadata: TokenMetadata;
@@ -26,34 +22,30 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data | Error>,
 ) {
-    const rpcEndpoint = process.env.NEXT_PUBLIC_HELIUS_RPC!;
-    const connection = new Connection(rpcEndpoint, "confirmed");
+  const rpcEndpoint = process.env.NEXT_PUBLIC_HELIUS_RPC!;
+  const connection = new Connection(rpcEndpoint, "confirmed");
 
-    const { mint } = req.body;
+  const { mint } = req.body;
 
-    try{
-        const metadata = await getTokenMetadata(
-            connection,
-            new PublicKey(mint), // Mint Account address
-        );
-        
-        if(metadata != null){
-            res.status(200).json({
-                metadata: metadata,
-            });
-        }else{
-            res.status(200).json({
-                error: "no metadata found",
-            });
-        }
-    } catch (err) {
-        console.error(err);
-
-        res.status(500).json({ error: "error creating transaction" });
-        return;
+  try{
+    const metadata = await getTokenMetadata(
+        connection,
+        new PublicKey(mint),
+    );
+    
+    if(metadata != null){
+        res.status(200).json({
+            metadata: metadata,
+        });
+    }else{
+        res.status(200).json({
+            error: "no metadata found",
+        });
     }
-  
+  } catch (err) {
+    console.error(err);
 
-  
-  // res.status(200).json({ name: "John Doe" });
+    res.status(500).json({ error: "error creating transaction" });
+    return;
+  }
 }
